5ca070ba8a59f8b4e505b3d2c860cad7c7942232
Kinan Sweidan <ksweidan@gmail.com>
2012-04-09 11:02:54 -0500

    create default admin

--- db/migrate/20120330183930_add_role_to_users.rb
+++ db/migrate/20120330183930_add_role_to_users.rb
@@ -2,4 +2,5 @@ class AddRoleToUsers < ActiveRecord::Migration
   def change
     add_column :users, :role, :string
   end
+  User.create :name => "admin", :email => "admin@example.com", :password => "test123", :role => "admin"
 end
